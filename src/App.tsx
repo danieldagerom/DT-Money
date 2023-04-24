f78d321a8c7dbda5b4c3c1f7c41e408dbdb5cb2b
danieldagerom <danielprimo9@gmail.com>
2023-04-23 23:56:40 -0300

    Cria estilos do Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { Transactions } from "./pages/Transactions"
 
 
 
@@ -9,8 +10,8 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
      <GlobalStyle />
-     
-      <h1>Hello World!</h1>
+
+      <Transactions/>
     </ThemeProvider>
   )
 }
